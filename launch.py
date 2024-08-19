24b8e186fb76f867dc0fbb2093e53d0ef5d1a5f1
HDRJ-One <98429490+HDRJ-One@users.noreply.github.com>
2024-08-19 01:55:32 -0400

    edit bibs

--- launch.py
+++ launch.py
@@ -242,10 +242,10 @@ def prepare_environment():
     run_pip(f"install -e {repo_dir('taming-transformers')}", "taming-transformers")
     git_clone(k_diffusion_repo, repo_dir('k-diffusion'), "K-diffusion", k_diffusion_commit_hash)
     run_pip(f"install -e {repo_dir('k-diffusion')}", "k-diffusion")
-    #git_clone(codeformer_repo, repo_dir('CodeFormer'), "CodeFormer", codeformer_commit_hash)
+    git_clone(codeformer_repo, repo_dir('CodeFormer'), "CodeFormer", codeformer_commit_hash)
     #run_pip(f"install -e {repo_dir('CodeFormer/basicsr')}", "basicsr")
     git_clone(blip_repo, repo_dir('BLIP'), "BLIP", blip_commit_hash)
-    run_pip(f"install -e {repo_dir('BLIP')}", "BLIP")
+    #run_pip(f"install -e {repo_dir('BLIP')}", "BLIP")
 
     print("Current working directory:", os.getcwd())
 	
